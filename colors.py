0aa1318146e0a78c7b16c9468b1eec978bb5e716
Kevin Froman <beardog@firemail.cc>
2017-12-28 14:20:39 -0500

    added colors

---
+++ colors.py
@@ -0,0 +1,10 @@
+class Colors:
+    def __init__(self):
+        self.PURPLE='\033[95m'
+        self.BLUE='\033[94m'
+        self.GREEN='\033[92m'
+        self.YELLOW='\033[93m'
+        self.RED='\033[91m'
+        self.BOLD='\033[1m'
+        self.UNDERLINE='\033[4m'
+        self.RESET="\x1B[m"
