a89435cfd3c45d01d4718318b34935b72c70d7c9
Kevin Froman <beardog@firemail.cc>
2018-01-14 22:30:42 -0600

    added setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+ setup(name='Onionr',
+  version='0.0.0',
+  description='P2P Microblogging Platform & Social network.',
+  author='Kevin Froman',
+  author_email='beardog@firemail.cc',
+  url='https://github.com/beardog108/onionr',
+  packages=['requests'],
+ )
