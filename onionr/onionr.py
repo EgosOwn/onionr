e2abf5f046f46aca4c48d880c2453378013c5367
Arinerron <arinesau@gmail.com>
2018-08-04 21:09:33 -0700

    Onionr UI updates

--- onionr/onionr.py
+++ onionr/onionr.py
@@ -201,7 +201,14 @@ class Onionr:
             'ui' : self.openUI,
             'gui' : self.openUI,
 
-            'getpassword': self.printWebPassword
+            'getpassword': self.printWebPassword,
+            'get-password': self.printWebPassword,
+            'getpwd': self.printWebPassword,
+            'get-pwd': self.printWebPassword,
+            'getpass': self.printWebPassword,
+            'get-pass': self.printWebPassword,
+            'getpasswd': self.printWebPassword,
+            'get-passwd': self.printWebPassword
         }
 
         self.cmdhelp = {
@@ -211,7 +218,7 @@ class Onionr:
             'start': 'Starts the Onionr daemon',
             'stop': 'Stops the Onionr daemon',
             'stats': 'Displays node statistics',
-            'getpassword': 'Displays the web password',
+            'get-password': 'Displays the web password',
             'enable-plugin': 'Enables and starts a plugin',
             'disable-plugin': 'Disables and stops a plugin',
             'reload-plugin': 'Reloads a plugin',
--- onionr/static-data/www/ui/src/js/main.js
+++ onionr/static-data/www/ui/src/js/main.js
@@ -95,14 +95,34 @@ String.prototype.replaceAll = function(search, replacement) {
     return target.split(search).join(replacement);
 };
 
-/* sanitizes HTML in a string */
-function encodeHTML(html) {
-    return String(html).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+/* useful functions to sanitize data */
+class Sanitize {
+    /* sanitizes HTML in a string */
+    static html(html) {
+        return String(html).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+    }
+
+    /* URL encodes a string */
+    static url(url) {
+        return encodeURIComponent(url);
+    }
 }
 
-/* URL encodes a string */
-function encodeURL(url) {
-    return encodeURIComponent(url);
+/* config stuff */
+function getWebPassword() {
+    return get("web-password", null);
+}
+
+function setWebPassword(password) {
+    return set("web-password", password);
+}
+
+function getTimingToken() {
+    return get("timing-token", null);
+}
+
+function setTimingToken(token) {
+    return set("timing-token", token);
 }
 
 /* user class */
@@ -159,13 +179,16 @@ class Post {
 
         var device = (jQuery(document).width() < 768 ? 'mobile' : 'desktop');
 
-        postTemplate = postTemplate.replaceAll('$user-name-url', encodeHTML(encodeURL(this.getUser().getName())));
-        postTemplate = postTemplate.replaceAll('$user-name', encodeHTML(this.getUser().getName()));
-        postTemplate = postTemplate.replaceAll('$user-id-url', encodeHTML(encodeURL(this.getUser().getID())));
-        postTemplate = postTemplate.replaceAll('$user-id-truncated', encodeHTML(this.getUser().getID().split('-').slice(0, 4).join('-')));
-        postTemplate = postTemplate.replaceAll('$user-id', encodeHTML(this.getUser().getID()));
-        postTemplate = postTemplate.replaceAll('$user-image', encodeHTML(this.getUser().getIcon()));
-        postTemplate = postTemplate.replaceAll('$content', encodeHTML(this.getContent()));
+        postTemplate = postTemplate.replaceAll('$user-name-url', Sanitize.html(Sanitize.url(this.getUser().getName())));
+        postTemplate = postTemplate.replaceAll('$user-name', Sanitize.html(this.getUser().getName()));
+        postTemplate = postTemplate.replaceAll('$user-id-url', Sanitize.html(Sanitize.url(this.getUser().getID())));
+
+        postTemplate = postTemplate.replaceAll('$user-id-truncated', Sanitize.html(this.getUser().getID().substring(0, 12) + '...'));
+        // postTemplate = postTemplate.replaceAll('$user-id-truncated', Sanitize.html(this.getUser().getID().split('-').slice(0, 4).join('-')));
+
+        postTemplate = postTemplate.replaceAll('$user-id', Sanitize.html(this.getUser().getID()));
+        postTemplate = postTemplate.replaceAll('$user-image', Sanitize.html(this.getUser().getIcon()));
+        postTemplate = postTemplate.replaceAll('$content', Sanitize.html(this.getContent()));
         postTemplate = postTemplate.replaceAll('$date-relative', timeSince(this.getPostDate(), device) + (device === 'desktop' ?  ' ago' : ''));
         postTemplate = postTemplate.replaceAll('$date', this.getPostDate().toLocaleString());
 
@@ -198,4 +221,199 @@ class Post {
     getPostDate() {
         return this.date;
     }
+}
+
+/* block class */
+class Block {
+    constructor(type, content) {
+        this.type = type;
+        this.content = content;
+    }
+
+    // returns the block hash, if any
+    getHash() {
+        return this.hash;
+    }
+
+    // returns the block type
+    getType() {
+        return this.type;
+    }
+
+    // returns the block header
+    getHeader(key, df) { // df is default
+        if(key !== undefined) {
+            if(this.getHeader().hasOwnProperty(key))
+                return this.getHeader()[key];
+            else
+                return (df === undefined ? null : df);
+        } else
+            return this.header;
+    }
+
+    // returns the block metadata
+    getMetadata(key, df) { // df is default
+        if(key !== undefined) {
+            if(this.getMetadata().hasOwnProperty(key))
+                return this.getMetadata()[key];
+            else
+                return (df === undefined ? null : df);
+        } else
+            return this.metadata;
+    }
+
+    // returns the block content
+    getContent() {
+        return this.content;
+    }
+
+    // returns the parent block's hash (not Block object, for performance)
+    getParent() {
+        if(!(this.parent instanceof Block) && this.parent !== undefined && this.parent !== null)
+            this.parent = Block.openBlock(this.parent); // convert hash to Block object
+        return this.parent;
+    }
+
+    // returns the date that the block was received
+    getDate() {
+        return this.date;
+    }
+
+    // returns a boolean that indicates whether or not the block is valid
+    isValid() {
+        return this.valid;
+    }
+
+    // returns a boolean thati ndicates whether or not the block is signed
+    isSigned() {
+        return this.signed;
+    }
+
+    // returns the block signature
+    getSignature() {
+        return this.signature;
+    }
+
+    // returns the block type
+    setType(type) {
+        this.type = type;
+        return this;
+    }
+
+    // sets block metadata by key
+    setMetadata(key, val) {
+        this.metadata[key] = val;
+        return this;
+    }
+
+    // sets block content
+    setContent(content) {
+        this.content = content;
+        return this;
+    }
+
+    // sets the block parent by hash or Block object
+    setParent(parent) {
+        this.parent = parent;
+        return this;
+    }
+
+    // indicates if the Block exists or not
+    exists() {
+        return !(this.hash === null || this.hash === undefined);
+    }
+
+    /* static functions */
+
+    // recreates a block by hash
+    static openBlock(hash) {
+        return parseBlock(response);
+    }
+
+    // converts an associative array to a Block
+    static parseBlock(val) {
+        var block = new Block();
+
+        block.type = val['type'];
+        block.content = val['content'];
+        block.header = val['header'];
+        block.metadata = val['metadata'];
+        block.date = new Date(val['date'] * 1000);
+        block.hash = val['hash'];
+        block.signature = val['signature'];
+        block.signed = val['signed'];
+        block.valid = val['valid'];
+        block.parent = val['parent'];
+
+        if(block.getParent() !== null) {
+            // if the block data is already in the associative array
+
+            /*
+            if (blocks.hasOwnProperty(block.getParent()))
+                block.setParent(Block.parseAssociativeArray({blocks[block.getParent()]})[0]);
+            */
+        }
+
+        return block;
+    }
+
+    // converts an array of associative arrays to an array of Blocks
+    static parseBlockArray(blocks) {
+        var outputBlocks = [];
+
+        for(var key in blocks) {
+            if(blocks.hasOwnProperty(key)) {
+                var val = blocks[key];
+
+                var block = Block.parseBlock(val);
+
+                outputBlocks.push(block);
+            }
+        }
+
+        return outputBlocks;
+    }
+
+    static getBlocks(args, callback) { // callback is optional
+        args = args || {}
+
+        var url = '/client/?action=searchBlocks&data=' + Sanitize.url(JSON.stringify(args)) + '&token=' + Sanitize.url(getWebPassword()) + '&timingToken=' + Sanitize.url(getTimingToken());
+
+        console.log(url);
+
+        var http = new XMLHttpRequest();
+
+        if(callback !== undefined) {
+            // async
+
+            http.addEventListener('load', function() {
+                callback(Block.parseBlockArray(JSON.parse(http.responseText)['blocks']));
+            }, false);
+
+            http.open('GET', url, true);
+            http.timeout = 5000;
+            http.send(null);
+        } else {
+            // sync
+
+            http.open('GET', url, false);
+            http.send(null);
+
+            return Block.parseBlockArray(JSON.parse(http.responseText)['blocks']);
+        }
+    }
+}
+
+/* temporary code */
+
+if(getWebPassword() === null) {
+    var password = "";
+    while(password.length != 64) {
+        password = prompt("Please enter the web password (run `./RUN-LINUX.sh --get-password`)");
+    }
+
+    setTimingToken(prompt("Please enter the timing token (optional)"));
+
+    setWebPassword(password);
+    window.location.reload(true);
 }
