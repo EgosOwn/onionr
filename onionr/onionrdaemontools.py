bed6475e1b5272166b8f48f69bb58db5055b0e49
Kevin <beardog@firemail.cc>
2018-08-31 19:51:14 -0500

    use secrets from dependencies

--- onionr/onionrdaemontools.py
+++ onionr/onionrdaemontools.py
@@ -17,7 +17,8 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
-import onionrexceptions, onionrpeers, onionrproofs, base64, logger, secrets
+import onionrexceptions, onionrpeers, onionrproofs, base64, logger
+from dependencies import secrets
 class DaemonTools:
     def __init__(self, daemon):
             self.daemon = daemon
