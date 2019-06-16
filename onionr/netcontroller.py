71805811cf3f44dd2e5c555e72b98c52a0e62a90
Kevin Froman <beardog@firemail.cc>
2019-06-16 13:20:54 -0500

    corrected permissions again

--- onionr/netcontroller.py
+++ onionr/netcontroller.py
@@ -145,7 +145,7 @@ HiddenServicePort 80 ''' + self.apiServerIP + ''':''' + str(self.hsPort)
                 elif 'opening socks listener' in line.decode().lower():
                     logger.debug(line.decode().replace('\n', ''))
             else:
-                logger.fatal('Failed to start Tor. Maybe a stray instance of Tor used by Onionr is still running?')
+                logger.fatal('Failed to start Tor. Maybe a stray instance of Tor used by Onionr is still running? This can also be a result of file permissions being too open')
                 return False
         except KeyboardInterrupt:
             logger.fatal('Got keyboard interrupt. Onionr will exit soon.', timestamp = False, level = logger.LEVEL_IMPORTANT)
