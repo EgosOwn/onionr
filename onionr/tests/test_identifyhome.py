405e8cdd24cec10d9cc2ef0beb9604b36b77ba78
Kevin Froman <beardog@firemail.cc>
2019-07-26 16:05:58 -0500

    added identify home test

---
+++ onionr/tests/test_identifyhome.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys, os
+sys.path.append(".")
+import unittest, uuid
+from utils import identifyhome
+
+class IdentifyHomeTest(unittest.TestCase):
+    def test_standard_linux(self):
+        del os.environ["ONIONR_HOME"]
+        self.assertEqual(identifyhome.identify_home(), os.path.expanduser('~') + '/.local/share/onionr/')
+    def test_environ(self):
+        os.environ["ONIONR_HOME"] = "testhome"
+        self.assertEqual(os.getcwd() + "/testhome/", identifyhome.identify_home())
+
+unittest.main()
