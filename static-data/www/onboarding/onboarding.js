265e2c181b2a879d7bd0917ee6b64af6043f0201
Kevin Froman <beardog@firemail.cc>
2019-10-17 05:00:24 -0500

    work on onboarding

--- static-data/www/onboarding/onboarding.js
+++ static-data/www/onboarding/onboarding.js
@@ -4,5 +4,23 @@ fetch('/getnewkeys', {
     }})
     .then((resp) => resp.text()) // Transform the data into text
     .then(function(resp) {
-        keys = keys.split('')
-    })
+        keys = resp.split('')
+})
+
+function getCheckValue(elName){
+    return document.getElementsByName(elName)[0].checked
+}
+
+document.getElementById('onboardingForm').onsubmit = function(e){
+    submitInfo = {}
+    submitInfo.massSurveil = getCheckValue('state')
+    submitInfo.stateTarget = getCheckValue('stateTarget')
+    submitInfo.localThreat = getCheckValue('local')
+    submitInfo.networkContrib = getCheckValue('networkContribution')
+    submitInfo.plainContrib = getCheckValue('networkContributionPlain')
+    submitInfo.donate = getCheckValue('donate')
+    submitInfo.deterministic = getCheckValue('useDeterministic')
+    submitInfo.mail = getCheckValue('useMail')
+
+    e.preventDefault()
+}
