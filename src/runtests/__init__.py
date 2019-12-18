9790785c08c8610f3b1306f3820d60e3a5d1c338
Kevin Froman <beardog@firemail.cc>
2019-12-17 19:31:41 -0600

    made runtests init flake8/pylint conforming

--- src/runtests/__init__.py
+++ src/runtests/__init__.py
@@ -3,6 +3,10 @@
 
     Test Onionr as it is running
 """
+import logger
+from onionrutils import epoch
+
+from . import uicheck, inserttest, stresstest
 """
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -17,18 +21,17 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-import logger
-from onionrutils import epoch
 
-from . import uicheck, inserttest, stresstest
+RUN_TESTS = [uicheck.check_ui,
+             inserttest.insert_bin_test,
+             stresstest.stress_test_block_insert]
 
-RUN_TESTS = [uicheck.check_ui, inserttest.insert_bin_test, stresstest.stress_test_block_insert]
 
 class OnionrRunTestManager:
     def __init__(self):
-        self.success:bool = True
-        self.run_date:int = 0
-    
+        self.success: bool = True
+        self.run_date: int = 0
+
     def run_tests(self):
         cur_time = epoch.get_epoch()
         logger.info(f"Doing runtime tests at {cur_time}")
@@ -39,4 +42,3 @@ class OnionrRunTestManager:
                 logger.info(last.__name__ + " passed")
         except ValueError:
             logger.error(last.__name__ + ' failed')
-    
