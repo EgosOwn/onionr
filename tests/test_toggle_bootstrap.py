34b1abddb7ad63ac4b06d2d6625bf046868d4f66
Kevin Froman <beardog@firemail.cc>
2019-12-29 11:33:47 -0600

    added toggle bootstrap command

--- tests/test_toggle_bootstrap.py
+++ tests/test_toggle_bootstrap.py
@@ -3,11 +3,13 @@ import sys, os
 sys.path.append(".")
 sys.path.append("src/")
 import unittest, uuid
-import config
 TEST_DIR = 'testdata/%s-%s' % (uuid.uuid4(), os.path.basename(__file__)) + '/'
 print("Test directory:", TEST_DIR)
 os.environ["ONIONR_HOME"] = TEST_DIR
 from onionrcommands import parser
+from onionrsetup import setup_config
+setup_config()
+import config
 class TestToggleBootstrap(unittest.TestCase):
     def test_toggle_bootstrap(self):
         testargs = ["onionr.py", "togglebootstrap"]
