85be8f76e507990ce26def27582e50c8b267b03a
Kevin Froman <beardog@firemail.cc>
2020-01-28 02:15:11 -0600

    improve formatting in removeblock and blacklist

--- src/onionrstorage/removeblock.py
+++ src/onionrstorage/removeblock.py
@@ -1,14 +1,34 @@
+"""Onionr - Private P2P Communication.
+
+remove onionr block from meta db
+"""
 import sys, sqlite3
 import onionrexceptions, onionrstorage
 from onionrutils import stringvalidators
 from coredb import dbfiles
 from onionrblocks import storagecounter
+"""
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 def remove_block(block):
-    '''
+    """
         remove a block from this node (does not automatically blacklist)
 
         **You may want blacklist.addToDB(blockHash)
-    '''
+    """
 
     if stringvalidators.validate_hash(block):
         conn = sqlite3.connect(dbfiles.block_meta_db, timeout=30)
