d4dd964069d653a9767fd627315ff53a8e2bc1c8
Kevin Froman <beardog@mailbox.org>
2020-02-02 19:07:48 -0600

    don't restart tor unless its our own

--- src/communicatorutils/restarttor.py
+++ src/communicatorutils/restarttor.py
@@ -1,5 +1,8 @@
 import netcontroller
+import config
+
 def restart(comm_inst):
-    net = comm_inst.shared_state.get(netcontroller.NetController)
-    net.killTor()
-    net.startTor()
+    if not config.get('tor.use_existing_tor', False):
+        net = comm_inst.shared_state.get(netcontroller.NetController)
+        net.killTor()
+        net.startTor()
