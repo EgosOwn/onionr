dee531fd51beabd61bd977a4cf53df0249dfd490
Kevin Froman <beardog@mailbox.org>
2020-02-08 17:07:39 -0600

    added license info to boxprint

--- src/utils/boxprint.py
+++ src/utils/boxprint.py
@@ -1,4 +1,12 @@
-def bordered(text):
+"""Add box around string.
+
+taken from https://stackoverflow.com/a/20757491 under https://creativecommons.org/licenses/by-sa/4.0/
+https://stackoverflow.com/users/816449/bunyk
+"""
+
+
+def bordered(text: str) -> str:
+    """Add border to string."""
     lines = text.splitlines()
     width = max(len(s) for s in lines)
     res = ['┌' + '─' * width + '┐']
