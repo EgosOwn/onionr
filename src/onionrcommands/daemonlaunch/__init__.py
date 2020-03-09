e4d4091e6674757820f0a00a07a0d4edc4a23cf2
Kevin Froman <beardog@mailbox.org>
2020-03-09 04:12:47 -0500

    fixed pep8 formatting in daemon launch

--- src/onionrcommands/daemonlaunch/__init__.py
+++ src/onionrcommands/daemonlaunch/__init__.py
@@ -100,7 +100,9 @@ def daemon():
     if config.get('general.display_header', True):
         logoheader.header("")
         if QUOTE[1]:
-            logger.info("\u001b[33m\033[F" + bordered(QUOTE[0] + '\n -' + QUOTE[1]), terminal=True)
+            logger.info(
+                "\u001b[33m\033[F" + bordered(QUOTE[0] + '\n -' + QUOTE[1]),
+                terminal=True)
         else:
             logger.info("\u001b[33m\033[F" + bordered(QUOTE[0]), terminal=True)
 
