68678177ed905405c4961e3f869b91b149c674d9
Kevin Froman <beardog@mailbox.org>
2020-03-16 02:55:36 -0500

    type hinted bytesconverter

--- src/onionrutils/bytesconverter.py
+++ src/onionrutils/bytesconverter.py
@@ -1,12 +1,14 @@
-def str_to_bytes(data):
-    '''Converts a string to bytes with .encode()'''
+def str_to_bytes(data: str) -> bytes:
+    '''Convert a string to bytes with .encode(), utf8'''
     try:
         data = data.encode('UTF-8')
     except AttributeError:
         pass
     return data
 
-def bytes_to_str(data):
+
+def bytes_to_str(data: bytes) -> str:
+    """Convert bytes to strings with .decode(), utf8"""
     try:
         data = data.decode('UTF-8')
     except AttributeError:
