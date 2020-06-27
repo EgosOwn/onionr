77800938988b87839659649bbbe9f6fc2027e1d8
Kevin <beardog@mailbox.org>
2020-06-27 18:00:31 -0500

    dev toggle scripts toggle animated background

--- scripts/disable-dev-config.py
+++ scripts/disable-dev-config.py
@@ -26,6 +26,7 @@ conf['transports']['tor'] = True
 conf['transports']['sneakernet'] = True
 conf['statistics']['i_dont_want_privacy'] = False
 conf['statistics']['server'] = ''
+conf['ui']['animated_background'] = True
 
 json.dump(conf, open('static-data/default_config.json', 'w'), sort_keys=True, indent=4)
 
