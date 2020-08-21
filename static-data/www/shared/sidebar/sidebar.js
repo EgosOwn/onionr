308fca7c22a030056f593653b0cde16bbdce6927
Kevin Froman <beardog@mailbox.org>
2020-08-21 09:57:59 -0500

    added creating blocks count to sidebar

--- static-data/www/shared/sidebar/sidebar.js
+++ static-data/www/shared/sidebar/sidebar.js
@@ -6,13 +6,39 @@ fetch('/shared/sidebar/sidebar.html', {
 .then((resp) => resp.text())
 .then(function(resp) {
     document.getElementById('sidebarContainer').innerHTML = resp
+    var quickviews = bulmaQuickview.attach()
 })
-
-
 
 window.addEventListener("keydown", function(event) {
     if (event.key === "s"){
-        let quickviews = bulmaQuickview.attach()
-        document.getElementsByClassName('sidebarBtn')[0].click()
+        if (document.activeElement.nodeName == "TEXTAREA" || document.activeElement.nodeName == "INPUT"){
+            if (! document.activeElement.hasAttribute("readonly")){
+                return
+            }
+        }
+        let refreshSideBar = function(){
+            if (document.hidden){return}
+            var existingValue = document.getElementById("insertingBlocks").innerText
+            fetch('/getgeneratingblocks', {
+                "method": "get",
+                headers: {
+                    "token": webpass
+                }})
+                .then((resp) => resp.text())
+                .then(function(resp) {
+                    console.debug(resp.length, existingValue)
+                    if (resp.length <= 2 && existingValue !== "0"){
+                        document.getElementById("insertingBlocks").innerText = "0"
+                        return
+                    }
+                    if (existingValue === resp.split(',').length){
+                        return
+                    }
+                    document.getElementById("insertingBlocks").innerText = resp.split(',').length - 1
+                })
+        }
+        setInterval(refreshSideBar, 3000)
+
+        setTimeout(function(){document.getElementsByClassName('sidebarBtn')[0].click()}, 300)
     }
 }, true)
