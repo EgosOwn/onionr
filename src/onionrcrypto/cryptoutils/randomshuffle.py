183e6491d0e9cbc18b949ec13d3e796b8ba884fa
Kevin Froman <beardog@mailbox.org>
2020-10-19 07:28:38 +0000

    use SystemRandom for randomshuffle instead of own rolled implementation

--- src/onionrcrypto/cryptoutils/randomshuffle.py
+++ src/onionrcrypto/cryptoutils/randomshuffle.py
@@ -1,13 +1,6 @@
-import secrets
+from random import SystemRandom
+
 def random_shuffle(theList):
     myList = list(theList)
-    shuffledList = []
-    myListLength = len(myList) + 1
-    while myListLength > 0:
-        removed = secrets.randbelow(myListLength)
-        try:
-            shuffledList.append(myList.pop(removed))
-        except IndexError:
-            pass
-        myListLength = len(myList)
-    return shuffledList
+    SystemRandom().shuffle(myList)
+    return myList
