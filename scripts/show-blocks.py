04105a2b706510d0559be55335a15c9f0548c973
Kevin Froman <beardog@mailbox.org>
2020-11-03 04:38:35 +0000

    show more info in show-blocks.py script

--- scripts/show-blocks.py
+++ scripts/show-blocks.py
@@ -6,6 +6,11 @@ if not os.path.exists('onionr.sh'):
     os.chdir('../')
 sys.path.append("src/")
 from coredb.blockmetadb import get_block_list
+from onionrblocks.onionrblockapi import Block
 
 for bl in get_block_list():
-    print(bl)
+    bl_obj = Block(bl, decrypt=False)
+    b_type = bl_obj.getType()
+    if not b_type:
+        b_type = "encrypted"
+    print(bl + " - " + str(bl_obj.date) + " - " + b_type)
