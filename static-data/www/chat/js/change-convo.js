0fb5e5df1094f6de4cec26ab57c08cd410427519
Kevin Froman <beardog@mailbox.org>
2020-12-29 07:58:44 +0000

    remove chat web files

--- static-data/www/chat/js/change-convo.js
+++
@@ -1,6 +0,0 @@
-var conversationListElements = document.getElementsByClassName('conversationList')
-for (i = 0; i < conversationListElements.length; i++){
-    conversationListElements[i].onclick = function(e){
-        
-    }
-}
