87685fd728efaf35d5df9d803cbe9600db4c34df
Kevin Froman <beardog@mailbox.org>
2021-01-15 21:36:57 +0000

    add license to disk ministry

--- src/bigbrother/ministry/ofdisk.py
+++ src/bigbrother/ministry/ofdisk.py
@@ -1,5 +1,23 @@
+"""Onionr - Private P2P Communication.
+
+Log (not block) read/write of non-user data files and non-python lib files
+"""
 from utils.identifyhome import identify_home
 import logger
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 
 
 def detect_disk_access(info):
