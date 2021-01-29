67d9dc355768512c3639c3c885c9b5c49399a151
Kevin Froman <beardog@mailbox.org>
2021-01-29 20:57:39 +0000

    added more modules to dependencycheck.py

--- src/etc/dependencycheck.py
+++ src/etc/dependencycheck.py
@@ -1,1 +1,13 @@
 from urllib3.contrib.socks import SOCKSProxyManager # noqa
+import gevent
+import flask
+import mimcvdf
+import toomanyobjs
+import stem
+import psutil
+import unpaddedbase32
+import niceware
+import watchdog
+import nacl
+import deadsimplekv
+import ujson
