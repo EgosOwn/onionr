061e2d1e01438c161540aea1d70d0ff5f5c4df9f
Kevin F <kevin@voidnet>
2022-02-04 00:18:57 -0600

    Added generic multiprocess wrapper Corrected blocks after timeout function

--- src/blockdb/__init__.py
+++ src/blockdb/__init__.py
@@ -18,10 +18,14 @@ def get_blocks_by_type(block_type):
         if block.type == block_type:
             yield block
 
+
 def get_blocks_after_timestamp(timestamp: int, block_type: bytes = ''):
     block_db = db.get_db_obj(block_db_path, 'u')
     for block_hash in db.list_keys(block_db_path):
         block = Block(block_hash, block_db[block_hash], auto_verify=False)
-        if block.type == block_type:
-            yield block
-
+        if block.timestamp > timestamp:
+            if block_type:
+                if block_type == block.type:
+                    yield block
+            else:
+                yield block
