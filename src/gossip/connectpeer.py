44f6b90777d38c7e558004912750ff59ffd73b9b
Kevin F <kevin@voidnet>
2022-02-27 13:34:16 -0600

    When checking for peer online to add to peer pool, make sure it closes and avoid attempting if its already in the pool

--- src/gossip/connectpeer.py
+++ src/gossip/connectpeer.py
@@ -1,11 +1,16 @@
+from gossip.commands import GossipCommands, command_to_byte
 import logger
 
 
 def connect_peer(peer_set, peer):
+    if peer in peer_set:
+        return
     try:
-        peer.get_socket()
+        s = peer.get_socket()
     except Exception:
         logger.warn(f"Could not connect to {peer.transport_address}")
     else:
+        s.sendall(command_to_byte(GossipCommands.CLOSE))
+        s.close()
         peer_set.add(peer)
         logger.info(f"connected to {peer.transport_address}")
