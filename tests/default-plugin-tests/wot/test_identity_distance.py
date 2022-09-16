fae9521d8f8a9021dcf9320edbc95bbabe8616d0
Kevin F <beardog@mailbox.org>
2022-09-15 20:01:59 -0500

    Made trust distance test graph generation reliable

--- tests/default-plugin-tests/wot/test_identity_distance.py
+++ tests/default-plugin-tests/wot/test_identity_distance.py
@@ -29,8 +29,14 @@ def generate_graph(iden: Identity, depth, max_neighbors):
 class IdentityDistanceTest(unittest.TestCase):
     def test_distance(self):
         iden = Identity(os.urandom(32), "1" + secrets.token_hex(4))
-        generate_graph(iden, 10, 5)
-        iden2 = list(list(iden.trusted)[0].trusted)[0]
+        while True:
+            generate_graph(iden, 10, 5)
+            try:
+                iden2 = list(list(iden.trusted)[0].trusted)[0]
+            except IndexError:
+                pass
+            else:
+                break
 
         self.assertEqual(get_distance(iden, iden2), 2)
 
